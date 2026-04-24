711b064133b142576a709a612b426365861ec256
Nisarg Patel <53168791+niss10@users.noreply.github.com>
2026-04-23 22:37:09 -0400

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let productOfNums = 1;
+        let productofNumsWOZero = 1;
+        let zeroCount = 0;
+
+        for (let num of nums){
+            if (num === 0){
+                zeroCount++;
+                if (zeroCount > 1){
+                    productOfNums = 0;
+                    productofNumsWOZero = 0;
+                    break;
+                }
+                productOfNums = 0;
+                continue;
+            }
+            productofNumsWOZero *= num; 
+        }
+        if (zeroCount === 0) productOfNums = productofNumsWOZero;
+        let output = []
+        for(let i = 0; i< nums.length;i++){
+            if(nums[i] === 0) {
+                output[i] =  productofNumsWOZero;
+                continue;
+            }
+            output[i] = productOfNums / nums[i];
+        }
+        return output
+    }
+}
