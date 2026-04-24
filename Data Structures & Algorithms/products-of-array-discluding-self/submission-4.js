9ab052912515dd85ef49b3826f1ffe2e277687ca
Nisarg Patel <53168791+niss10@users.noreply.github.com>
2026-04-24 11:10:10 -0400

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    // In this approach we will solve this problem y using prefix sum/product and suffix sum/product
+    // Prefix product is product of all elements before present element
+    // Sufix product is product of all elements after preset element
+    productExceptSelf(nums) {
+      let output = [];
+      let prefix = 1;
+      for(let i = 0;i<nums.length;i++){
+        output[i] = prefix;
+        prefix *= nums[i];
+      }
+
+      let suffix = 1;
+      for (let i = nums.length-1; i>=0; i--){
+        output[i] *= suffix;
+        suffix *= nums[i];
+      }
+
+      return output;
+    }
+}
