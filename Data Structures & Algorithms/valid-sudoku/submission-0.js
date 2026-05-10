c6c41ab5eb2bc97b2e32f6b40493fb4f5214ec9a
Nisarg Patel <53168791+niss10@users.noreply.github.com>
2026-05-10 09:56:05 -0400

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        const validSudokuRow = new Map();
+        const validSudokuCol = new Map();
+        const validSudokuSquare = new Map();
+
+        for(let row = 0; row < board.length; row++){
+            if(!validSudokuRow.has(row)){
+                validSudokuRow.set(row, new Set())
+            }
+            const rowStats = validSudokuRow.get(row);
+            for(let col = 0; col < board[row].length; col++){
+                if(!validSudokuCol.has(col)){
+                    validSudokuCol.set(col, new Set());
+                }
+                const colStats = validSudokuCol.get(col);
+                const squareKey = Math.floor(row/3) + '-' + Math.floor(col/3);
+                if(!validSudokuSquare.has(squareKey)){
+                    validSudokuSquare.set(squareKey, new Set());
+                }
+                const squareStats = validSudokuSquare.get(squareKey);
+                const element = board[row][col];
+                if(element === ".") continue;
+
+                if(rowStats.has(element) || colStats.has(element) || squareStats.has(element)) return false;
+
+                rowStats.add(element);
+                colStats.add(element);
+                squareStats.add(element);
+                
+            }
+        }
+        return true
+    }
+}
